7628a7547892ffd889890d15765a60fdcedca790
Javier Alaves <jayalaves@gmail.com>
2021-04-04 08:35:47 +0200

    Dynamic routing and more

--- components/layout.js
+++ components/layout.js
@@ -4,8 +4,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[Your Name]'
-export const siteTitle = 'Next.js Sample Website'
+const name = 'Javier Alaves'
+export const siteTitle = 'Javier Alaves'
 
 export default function Layout({ children, home }) {
   return (
